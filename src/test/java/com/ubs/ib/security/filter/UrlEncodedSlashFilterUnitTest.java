8afd2f6bf2a9867778035c9665f56f36a88c2fa1
ashishaggarwal92 <ashishaggarwal.92@gmail.com>
2025-06-20 11:40:12 +0530

    Security

--- src/test/java/com/ubs/ib/security/filter/UrlEncodedSlashFilterUnitTest.java
+++ src/test/java/com/ubs/ib/security/filter/UrlEncodedSlashFilterUnitTest.java
@@ -2,30 +2,34 @@ package com.ubs.ib.security.filter;
 
 import com.ub.ib.security.filter.UrlEncodedSlashWebFilter;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.WebFilter;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 
 @WebFluxTest
 public class UrlEncodedSlashFilterUnitTest {
 
-    private final WebTestClient webTestClient;
+    @Autowired
+    private WebTestClient webTestClient;
 
-    UrlEncodedSlashFilterUnitTest(WebTestClient webTestClient) {
-        this.webTestClient = webTestClient;
-    }
 
     @Test
     void shouldAllowEncodedSlashEverywhere() {
         webTestClient.get()
-                .uri("/any/path%2Fwith%2Fencoded")
+                .uri("/api/test%2F123")
                 .exchange()
                 .expectStatus().isOk()
-                .expectBody(String.class).isEqualTo("OK");
+                .expectBody(String.class)
+                .isEqualTo("Handled: test/123");
     }
 
     @Configuration
@@ -43,9 +47,9 @@ public class UrlEncodedSlashFilterUnitTest {
 
     @RestController
     static class DummyController {
-        @GetMapping("/any/{path}")
-        public String handler() {
-            return "OK";
+        @GetMapping("/api/{id}")
+        public String handle(@PathVariable String id) {
+            return "Handled: " + URLDecoder.decode(id, StandardCharsets.UTF_8);
         }
     }
 }
